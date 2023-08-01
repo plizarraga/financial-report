72ec11eb1aa0d718d00e0025cb73ee2dc008b18f
Alexander Ruiz Gonzalez <alexander.ruiz@publicissapient.com>
2023-08-01 17:58:52 -0600

    Added sass and bootstrap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,10 @@ const Home = () => {
     <>
       <h1>Home</h1>
       <NavLink to="/about">About</NavLink>
+      <br />
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
     </>
   );
 };
