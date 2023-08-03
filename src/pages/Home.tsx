bdaa7fcddc29b4d526286658cfe7ca87e6d2a1e3
Alexander Ruiz Gonzalez <alexander.ruiz@publicissapient.com>
2023-08-03 14:26:34 -0600

    Added react spreadsheet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,31 @@
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import Spreadsheet from 'react-spreadsheet';
+import { read, utils } from 'xlsx';
 
-const Home = () => {
+const Home: React.FC = () => {
+  const [tableData, setTableData] = useState<[][] | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = new Uint8Array(e.target?.result as ArrayBuffer);
+        const workbook = read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = utils.sheet_to_json(worksheet, { header: 1 });
+        setTableData(jsonData);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
   return (
-    <>
-      <h1>Home</h1>
-      <NavLink to="/about">About</NavLink>
-      <br />
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-    </>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      {tableData && <Spreadsheet data={tableData} />}
+    </div>
   );
 };
 
