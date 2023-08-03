5675af06d5d702598626faa61a3d9c07fae8adcc
Pedro Lizarraga <plizarraga86@gmail.com>
2023-08-02 19:22:24 -0700

    removed coverage

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     setupFiles: ['./vitestSetup.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'html'],
     },
   },
 });
